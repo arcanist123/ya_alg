f1534f11ccd70c06407cdf3591fb86bee23c07dc
ARcc <dmitry.yudin.a@gmail.com>
2024-03-22 18:46:33 +0100

    test

--- season05/w1/f/src/main.rs
+++ season05/w1/f/src/main.rs
@@ -1,3 +1,25 @@
+use std::fs::File;
+use std::io::BufRead;
+
 fn main() {
-    println!("Hello, world!");
+    // Define the file path
+    let file_path = "input.txt";
+
+    // Open the file in read mode and handle potential errors
+    let file = match File::open(file_path) {
+        Ok(file) => file,
+        Err(err) => panic!("Error opening file: {}", err),
+    };
+
+    // Create a buffered reader
+    let reader = std::io::BufReader::new(file);
+
+    // Collect lines from the reader into a vector of strings
+    let lines: Vec<String> = reader
+        .lines()
+        .map(|line| line.expect("Error reading line"))
+        .collect();
+
+    let mut number_of_components = 0;
+    for (current_line_number, line) in lines.iter().take(2).enumerate() {}
 }

025742b2ec0c9508ecee5fb3c8bf0cb937e6582c
ARcc <dmitry.yudin.a@gmail.com>
2024-04-16 09:18:16 +0200

    solved first test case

--- season05/w1/h/src/main.rs
+++ season05/w1/h/src/main.rs
@@ -43,7 +43,7 @@ fn main() {
         if position1 == position2 {
             time = 0f64;
         } else {
-            time = -1f64;
+            time = f64::MAX;
         }
     } else if speed1 == 0 {
         time = RunState::create_from_i32(speed2, position2, speed1, position1, length).get_time();
@@ -51,9 +51,13 @@ fn main() {
         time = RunState::create_from_i32(speed1, position1, speed2, position2, length).get_time();
     }
 
-    // let state = RunState::new(0f64, 0f64, 0f64, 0f64, 0f64);
-
-    println!("{}", time);
+    if time == f64::MAX {
+        println!("NO");
+    } else {
+        println!("YES");
+        let time_as_string = time.to_string().chars().take(12).collect::<String>();
+        println!("{}", time_as_string)
+    }
 }
 
 #[derive(Debug)]
@@ -67,19 +71,9 @@ struct RunState {
 
 impl RunState {
     fn transition_state(&self) -> Self {
-        let time = match self.speed2 {
-            x if x < 0f64 => f64::min(
-                (1f64 - self.position1) / self.speed1,
-                self.position2 / self.speed2,
-            ),
-            x if x == 0f64 => (1f64 - self.position1) / self.speed1,
-            x if x > 0f64 => f64::min(
-                (1f64 - self.position1) / self.speed1,
-                (1f64 - self.position2) / self.speed2,
-            ),
-            _ => 2f64,
-        };
-
+        let time1 = RunState::get_time_until_border(self.speed1, self.position1);
+        let time2 = RunState::get_time_until_border(self.speed2, self.position2);
+        let time = f64::min(time1, time2);
         Self {
             speed1: self.speed1,
             position1: self.position1 + (self.speed1 * time),
@@ -124,10 +118,18 @@ impl RunState {
     }
     fn inverse_state(&self) -> Self {
         Self {
-            speed1: 0f64 - self.speed1,
-            position1: 1f64 - self.position1,
-            speed2: 0f64 - self.speed2,
-            position2: 1f64 - self.position2,
+            speed1: if RunState::is_at_border(self.position1) {
+                0f64 - self.speed1
+            } else {
+                self.speed1
+            },
+            position1: self.position1,
+            speed2: if RunState::is_at_border(self.position2) {
+                0f64 - self.speed2
+            } else {
+                self.speed2
+            },
+            position2: self.position2,
             time: self.time,
         }
     }
@@ -150,7 +152,7 @@ impl RunState {
                 delta_position / (self.speed1 + self.speed2)
             }
         } else {
-            next_state.get_time()
+            next_state.inverse_state().get_time()
         }
     }
     fn get_number_at_precision(value: f64) -> i64 {
@@ -162,6 +164,19 @@ impl RunState {
         } else {
             start_1 <= end_2
         }
-        // core::cmp::max(start_1, start_2) >= core::cmp::min(end_1, end_2)
+    }
+
+    fn is_at_border(position: f64) -> bool {
+        RunState::get_number_at_precision(position) == 0
+            || RunState::get_number_at_precision(position) == 10000000000
+    }
+
+    fn get_time_until_border(speed: f64, position: f64) -> f64 {
+        match speed {
+            x if x < 0f64 => (0f64 - position) / speed,
+            x if x == 0f64 => f64::MAX,
+            x if x > 0f64 => (1f64 - position) / speed,
+            _ => f64::MAX,
+        }
     }
 }
